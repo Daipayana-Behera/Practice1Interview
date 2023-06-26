bdfcf404889fcf57d228a118f79acb890877bbc8
dikud <dikud@AVB-14>
2023-06-26 19:46:40 +0530

    Add project files.

---
+++ Constructor2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankA bankA = new BankA(100);
+            BankB bankB = new BankB(200);
+            BankC bankC = new BankC(300);
+
+            decimal balanceA = bankA.GetBalance();
+            decimal balanceB = bankB.GetBalance();
+            decimal balanceC = bankC.GetBalance();
+
+            Console.WriteLine("BankA balance: $" + balanceA);
+            Console.WriteLine("BankB balance: $" + balanceB);
+            Console.WriteLine("BankC balance: $" + balanceC);
+            Console.ReadLine();
+        }
+    }
+
+abstract class Bank
+    {
+        public abstract decimal GetBalance();
+    }
+
+    class BankA : Bank
+    {
+        private decimal balance;
+
+        public BankA(decimal initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public override decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+
+    class BankB : Bank
+    {
+        private decimal balance;
+
+        public BankB(decimal initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public override decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+
+    class BankC : Bank
+    {
+        private decimal balance;
+
+        public BankC(decimal initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public override decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+
+
+
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Patient p = new Patient("Khushi",23,"Cold");
+           Console.ReadLine();
+        }
+    }
+    public class Patient
+    {
+        public string Name;
+        public int Age;
+        public string Disease;
+
+        public Patient(string n, int a, string d)
+        {
+            Name = n;
+            Age = a;
+            Disease = d;
+            
+            //constructor does't have any return type.
+            //For printing data we use this 
+            Console.Write(Name);
+            Console.Write(',');
+            Console.Write(Age);
+            Console.Write(',');
+            Console.Write(Disease);
+        }
+    }
+}
